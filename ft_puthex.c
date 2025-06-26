0fd8c9f971216176245b0b3d475ca77e82e1ada6
Sora <34161147+suatkvam@users.noreply.github.com>
2025-06-26 07:49:18 +0300

    Update ft_puthex.c

--- ft_puthex.c
+++ ft_puthex.c
@@ -2,12 +2,11 @@
 
 void	ft_puthex(unsigned int number, int *len, char *base)
 {
-	if (number >= 16)
-	{
 		if (number >= 16)
+{
 			ft_puthex(number / 16, len, base);
 		ft_putchar(base[number % 16], len);
-	}
+}
 	else
 		ft_putchar(base[number % 16], len);
 }
